06677c54d1be1099081582cce209a39b063ab3fe
boluwatifeajayi <bolu4good@gmail.com>
2023-06-20 23:00:27 +0100

    shhh

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,48 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const manifestForPlugin = {
-	registerType: "prompt",
-	includeAssets: ["prime.png", "prime.png", "prime.png"],
-	manifest: {
-		name: "Prime",
-		short_name: "Prime",
-		description: "An app for manageing finances",
-		icons: [
-			{
-				src: "/prime.png",
-				sizes: "192x192",
-				type: "image/png",
-			},
-			{
-				src: "/prime.png",
-				sizes: "512x512",
-				type: "image/png",
-			},
-			{
-				src: "/prime.png",
-				sizes: "180x180",
-				type: "image/png",
-				purpose: "apple touch icon",
-			},
-			{
-				src: "/prime.png",
-				sizes: "225x225",
-				type: "image/png",
-				purpose: "any maskable",
-			},
-		],
-		theme_color: "#171717",
-		background_color: "#e8ebf2",
-		display: "standalone",
-		scope: "/",
-		start_url: "/",
-		orientation: "portrait",
-	},
-};
-
+// https://vitejs.dev/config/
 export default defineConfig({
-	base: "./",
-	plugins: [react(), VitePWA(manifestForPlugin)],
-});
+  plugins: [react()],
+  server: {
+    port: 3000,
+  }
+})
